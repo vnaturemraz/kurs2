0ab64fa74a1336dab4c92decef529f09bd3a282c
Алина <136386537+vnaturemraz@users.noreply.github.com>
2023-06-24 21:45:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+// //1 задание
+//let a = 10;
+//alert(a);
+//a=20;
+//alert(a);
+//
+// // 2
+//let appleAge = 2007;
+//alert(appleAge);
+
+// // 3
+//let nameCreator="Брендан Эйх";
+//alert(nameCreator)
+
+// //4
+//let a=10;
+//let b=2;
+//alert(a+b);
+//alert(a-b);
+//alert(a*b);
+//alert(a/b);
+
+// //5
+//let a=2;
+//result=a**5;
+//alert(result);
+
+// //6
+//let a=9;
+//let b=2;
+//alert(a%b);
+
+// //7
+//let num=1;
+//num+=5;
+//num-=3;
+//num*=7;
+//num/=3;
+//++num;
+//--num;
+//alert(num)
+
+// //8
+//let age = prompt('Сколько вам лет?');
+//alert(age)
+
+// //9.0
+//let user = {
+//    name: "Alex",
+//    age: 25,
+//    isAdmin: true,
+//}
+
+// //9.1
+//user['city of residence']="London";
+
+// //9.2
+//user.age=20;
+
+// //9.3
+//delete user['city of residence'];
+
+// //9.4
+//let info = prompt("Какую информацию хотите узнать о пользователе?");
+//alert(user[info]);
+
+// //10
+// let userName = prompt('Как вас зовут?');
+//alert(`Привет, ${userName}!`);
+
