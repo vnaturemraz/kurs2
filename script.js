f0d895446c7664aaddbd32e677c462f8cce4bfae
vnaturemraz <vnaturemrazz@gmail.com>
2023-07-25 18:40:05 +0300

    hw2.4

---
+++ script.js
@@ -0,0 +1,45 @@
+// 1
+// for (let i = 0; i < 2; i++) {
+//     console.log("Привет!");
+// }
+
+//2
+// for (let i= 1; i<6; i++) {
+//     console.log(i)
+// }
+
+//3
+// for (let i= 7; i<23; i++) {
+//     console.log(i)
+// }
+
+//4
+// const user = {
+//     "Коля" :"200",
+//     "Вася" :"300",
+//     "Петя" :"400",
+// }
+
+// for(let money in user){
+//     console.log(`${money}- зарплата ${user[money]} долларов`);
+// }
+
+//5
+// let n=1000;
+// let sum=0;
+
+// while (n>50) {
+//     n=n/2;
+//     sum++;
+// }
+
+// console.log(n);
+// console.log(sum);
+
+//6
+// let friday=5;
+
+// for (; friday <=31; friday+=7) {
+//     console.log(`Сегодня пятница, ${friday}-е число. Необходимо подготовить отчет.`)
+    
+// }
